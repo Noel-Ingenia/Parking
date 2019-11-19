838f7a80bba25ec07433a646d4adc67bc9c458c7
Noel-Ingenia <57939644+Noel-Ingenia@users.noreply.github.com>
2019-11-19 11:20:13 +0100

    Empezando Crud

--- routes/accounts.js
+++ routes/accounts.js
@@ -4,14 +4,38 @@ const sql = require('mssql');
 var conection = require('./conection');
 
 /* GET lista de usuarios */
+// router.get('/', function(req, res, next) {
+//   sql.connect(conection, err => {
+//     new sql.Request().query('select * from TablaPrueba', (err, result) => {
+//         console.dir(result.recordset);
+//         res.json(result.recordset);
+//         sql.close();
+//     });
+//   })
+// });
+
+/* GET Usuario por ID */
 router.get('/', function(req, res, next) {
-  sql.connect(conection, err => {
-    new sql.Request().query('select * from TablaPrueba', (err, result) => {
-        console.dir(result.recordset);
-        res.json(result.recordset);
-        sql.close();
+  if (req.query.id){
+    sql.connect(conection, err => {
+      new sql.Request()
+        .input('value', sql.Int, req.query.id) //AQUI VA EL ID A BUSCAR
+        .query('select * from TablaPrueba where id = @value', (err, result) => {
+          console.dir(result.recordset);
+          res.json(result.recordset);
+          sql.close();
+      });
     });
-  });
+  } else {
+    sql.connect(conection, err => {
+      new sql.Request()
+        .query('select * from TablaPrueba', (err, result) => {
+          console.dir(result.recordset);
+          res.json(result.recordset);
+          sql.close();
+      });
+    });
+  }
 });
 
 module.exports = router;
